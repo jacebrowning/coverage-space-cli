c334b124aa9fa8bbd0cd35c206e0ef4a980a7219
Jace Browning <jacebrowning@gmail.com>
2016-08-22 19:10:58 -0400

    Support Python 3

--- coveragespace/cache.py
+++ coveragespace/cache.py
@@ -12,21 +12,27 @@ class Cache(object):
 
     def _load(self):
         try:
-            with open(self.PATH, 'r') as fin:
-                data = pickle.load(fin)
-        except (IOError, KeyError, IndexError):
-            pass
-        else:
-            if isinstance(data, dict):
-                self._data = data
+            with open(self.PATH, 'rb') as fin:
+                text = fin.read()
+        except IOError:
+            text = None
+
+        try:
+            data = pickle.loads(text)
+        except (TypeError, KeyError, IndexError):
+            data = None
+
+        if isinstance(data, dict):
+            self._data = data
 
     def _store(self):
         directory = os.path.dirname(self.PATH)
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        with open(self.PATH, 'w') as fout:
-            pickle.dump(self._data, fout)
+        text = pickle.dumps(self._data)
+        with open(self.PATH, 'wb') as fout:
+            fout.write(text)
 
     def set(self, url, data, response):
         slug = self._slugify(url, data)
