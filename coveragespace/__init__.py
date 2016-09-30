2ea8fa8d0f00e8b9b52e6af9fdfeb1db7dcd0787
Jace Browning <jacebrowning@gmail.com>
2016-09-29 22:56:01 -0400

    Use SSL for API calls

--- coveragespace/__init__.py
+++ coveragespace/__init__.py
@@ -5,6 +5,6 @@ import sys
 __project__ = 'coverage.space'
 __version__ = '0.6.1'
 
-API = 'http://api.coverage.space'
+API = 'https://api.coverage.space'
 
 VERSION = "{0} v{1}".format(__project__, __version__)
